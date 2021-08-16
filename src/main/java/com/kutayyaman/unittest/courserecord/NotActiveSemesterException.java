c0ff9e1782163bcfd246da1171028f177b7b2518
Kutay Yaman <yamankutay1@gmail.com>
2021-08-16 10:28:04 +0300

    add a lecture to a lecturer test

---
+++ src/main/java/com/kutayyaman/unittest/courserecord/NotActiveSemesterException.java
@@ -0,0 +1,7 @@
+package com.kutayyaman.unittest.courserecord;
+
+public class NotActiveSemesterException extends RuntimeException {
+    public NotActiveSemesterException(String message) {
+        super(message);
+    }
+}
