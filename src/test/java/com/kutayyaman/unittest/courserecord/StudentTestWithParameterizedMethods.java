41a963fd0bb96aa7d404bf32e440d7788dbb8331
Kutay Yaman <yamankutay1@gmail.com>
2021-08-24 19:52:21 +0300

    @ArgumentSource

--- src/test/java/com/kutayyaman/unittest/courserecord/StudentTestWithParameterizedMethods.java
+++ src/test/java/com/kutayyaman/unittest/courserecord/StudentTestWithParameterizedMethods.java
@@ -211,4 +211,48 @@ public class StudentTestWithParameterizedMethods {
 
     }
 
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
+    class ArgumentsSourceDemo {
+        private int studentCourseSize = 0;
+
+        @BeforeAll
+        void setUp() {
+            student = new Student("id1", "Ahmet", "Yilmaz");
+        }
+
+        @ParameterizedTest
+        @ArgumentsSource(CourseCodeAndTypeProvider.class)
+        void addCourseToStudent(String courseCode, Course.CourseType courseType) {
+
+            final Course course = new Course(courseCode);
+            course.setCourseType(courseType);
+            final LecturerCourseRecord lecturerCourseRecord = new LecturerCourseRecord(course, new Semester());
+            student.addCourse(lecturerCourseRecord);
+            studentCourseSize++;
+            assertEquals(studentCourseSize, student.getStudentCourseRecords().size());
+            assertTrue(student.isTakeCourse(new Course(courseCode)));
+            assumingThat(courseCode.equals("101"),
+                    () -> assertEquals(Course.CourseType.MANDATORY, courseType)
+            );
+            assumingThat(courseCode.equals("103"),
+                    () -> assertEquals(Course.CourseType.ELECTIVE, courseType)
+            );
+            assumingThat(courseCode.equals("105"),
+                    () -> assertEquals(Course.CourseType.MANDATORY, courseType)
+            );
+        }
+    }
+
+    static class CourseCodeAndTypeProvider implements ArgumentsProvider {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context) throws Exception {
+            return Stream.of(
+                    Arguments.of("101", Course.CourseType.MANDATORY),
+                    Arguments.of("103", Course.CourseType.ELECTIVE),
+                    Arguments.of("105", Course.CourseType.MANDATORY)
+
+            );
+        }
+    }
 }
